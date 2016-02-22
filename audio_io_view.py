d3f593c032ea4c1b73f7358f5bb758a57f01371b
Shoichi Koyama <koyama@ravel3.210.11.133.in-addr.arpa>
2016-02-22 22:36:54 +0900

    mod io_view

--- audio_io_view.py
+++ audio_io_view.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
 
+import sys
+import codecs
 import pyaudio
+
+sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
 
 def audio_dump():
     pa = pyaudio.PyAudio()
     num_api = pa.get_host_api_count()
     num_dev = pa.get_device_count()
 
-    api_info = [[]]
+    api_info = []
     for i in range(0,num_api):
-        api_info[i] = pa.get_host_api_info_by_index(i)
+        api_info.append(pa.get_host_api_info_by_index(i))
 
     for i in range(0,num_dev):
         dev_info = pa.get_device_info_by_index(i)
         print "Index: %d" % i
-        print " -DeviceName: %s\n -HostName: %s\n -SampleRate: %dHz\n -MaxInputChannels: %d\n -MaxOutputChannels: %d" % (dev_info['name'], api_info[dev_info['hostApi']]['name'], int(dev_info['defaultSampleRate']), int(dev_info['maxInputChannels']), int(dev_info['maxOutputChannels']))
+        print " -DeviceName: %s" % dev_info['name']
+        print " -HostName: %s" % api_info[int(dev_info['hostApi'])]['name']
+        print " -SampleRate: %dHz\n -MaxInputChannels: %d\n -MaxOutputChannels: %d" % (int(dev_info['defaultSampleRate']), int(dev_info['maxInputChannels']), int(dev_info['maxOutputChannels']))
 
 audio_dump()
